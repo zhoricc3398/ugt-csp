822f88a210f255466cd83fc6b27cf38c2675cbb1
tskhvariashvili <tskhvariashvili.davit@gmail.com>
2021-03-09 14:54:53 +0400

    fix

--- modules/serviceWorker_example/serviceWorker.js
+++ modules/serviceWorker_example/serviceWorker.js
@@ -1,6 +1,6 @@
 const staticDevCoffee = "dev-coffee-site-v1"
 const assets = [
-  "/",
+  "https://zhoricc3398.github.io/ugt-csp/modules/serviceWorker_example/",
   "https://zhoricc3398.github.io/ugt-csp/modules/serviceWorker_example/index.html",
   "https://zhoricc3398.github.io/ugt-csp/modules/serviceWorker_example/css/style.css",
   "https://zhoricc3398.github.io/ugt-csp/modules/serviceWorker_example/js/app.js",
@@ -12,7 +12,7 @@ const assets = [
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
     caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets)
+      cache.addAll(assets);
     })
   )
 })
