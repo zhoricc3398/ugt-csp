4ff2d4f52d32e88291ec3e49da9ddf36ebc87b8d
tskhvariashvili <tskhvariashvili.davit@gmail.com>
2021-03-09 14:44:13 +0400

    fix

--- modules/serviceWorker_example/js/app.js
+++ modules/serviceWorker_example/js/app.js
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", showCoffees);
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("/serviceWorker.js")
+      .register("https://zhoricc3398.github.io/ugt-csp/modules/serviceWorker_example/serviceWorker.js")
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err))
   })
