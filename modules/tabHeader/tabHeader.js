17b0ccc6ef4c3c9df0ca51c2625c5bba87a78390
tskhvariashvili <tskhvariashvili.davit@gmail.com>
2021-03-16 13:48:07 +0400

    .

---
+++ modules/tabHeader/tabHeader.js
@@ -0,0 +1,12 @@
+var tab_headerContainer = document.getElementsByClassName('.fixed-top')[0];
+var tab_elemContainer = `
+    <div id="ugt-tab-header" class="ugt-tab-header">
+        <div class="container">
+            <ul>
+                <li><a href='https://ugt.ge/'>ugt</a></li>
+                <li><a href='https://ugtcloud.ge/'>service</a></li>
+                <li><a href='https://cloud.ugt.ge/' class="active">online shopping</a></li>
+            </ul>
+        </div>
+    </div>`;
+    tab_headerContainer.appendChild(tab_elemContainer);
