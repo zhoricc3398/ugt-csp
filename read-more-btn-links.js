7e59dfcfd8ddb7866e40c6f2691309d6572e7c80
zhoricc <giorgi.sharabidze@ugt.ge>
2021-04-05 14:35:29 +0400

    add links for read more btn

---
+++ read-more-btn-links.js
@@ -0,0 +1,10 @@
+const links = [
+    {
+        on: 'https://ugt.ge/ge/services/consulting/business-continuity',
+        to: 'https://ugtcloud.ge/ge/services/biznes-utsqvetobis-martvis-sistemis-danergva'
+    },
+    {
+        on: 'https://ugt.ge/ge/services/consulting/shepaseba',
+        to: 'https://ugtcloud.ge/ge/services/shefaseba-saertashoriso-standartebisa-da-metodologiebis-mikhedvit'
+    }
+];
